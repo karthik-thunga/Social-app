c4db1cf0fdd517be1c03924a85d1086aae79309f
karthik <kota.karthikthunga803@gmail.com>
2020-05-18 19:27:36 +0530

    Login,Logout,Update Password, Reset password added

--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+# Creating dashboard view
+
+@login_required
+def dashboard(request):
+    return render(
+        request, 'accounts/dashboard.html', { 'section':'dashboard' }
+    )
